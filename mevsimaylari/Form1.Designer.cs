a80be8918cfdd737befd8e135c3d7d8a53f97d7c
goknurbilisim1 <145705901+goknurbilisim1@users.noreply.github.com>
2023-10-16 16:29:50 +0300

    Add files via upload

---
+++ mevsimaylari/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace mevsimaylari
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMevsim = new System.Windows.Forms.Label();
+            this.cmbAylar = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMevsim
+            // 
+            this.lblMevsim.AutoSize = true;
+            this.lblMevsim.Location = new System.Drawing.Point(332, 52);
+            this.lblMevsim.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMevsim.Name = "lblMevsim";
+            this.lblMevsim.Size = new System.Drawing.Size(51, 20);
+            this.lblMevsim.TabIndex = 0;
+            this.lblMevsim.Text = "label1";
+            // 
+            // cmbAylar
+            // 
+            this.cmbAylar.FormattingEnabled = true;
+            this.cmbAylar.Items.AddRange(new object[] {
+            "Ocak",
+            "Şubat",
+            "Mart",
+            "Nisan",
+            "Mayıs",
+            "Haziran",
+            "Temmuz",
+            "Ağustos",
+            "Eylül",
+            "Ekim",
+            "Kasım",
+            "Aralık"});
+            this.cmbAylar.Location = new System.Drawing.Point(62, 52);
+            this.cmbAylar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbAylar.Name = "cmbAylar";
+            this.cmbAylar.Size = new System.Drawing.Size(180, 28);
+            this.cmbAylar.TabIndex = 1;
+            this.cmbAylar.SelectedIndexChanged += new System.EventHandler(this.cmbAylar_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(306, 90);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(177, 151);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 431);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cmbAylar);
+            this.Controls.Add(this.lblMevsim);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMevsim;
+        private System.Windows.Forms.ComboBox cmbAylar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ mevsimaylari/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mevsimaylari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmbAylar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            int secim = cmbAylar.SelectedIndex;
+
+            if (secim == 2 || secim == 3 || secim == 4) //Mart Nisan Mayıs
+            {
+                lblMevsim.Text = "İlkbahar";
+                pictureBox1.Image=Properties.Resources.ilkbahar;
+            }
+            else if (secim == 5 || secim == 6 || secim == 7) //Haziran Temmuz Ağustos
+            {
+                lblMevsim.Text = "Yaz";
+                pictureBox1.Image = Properties.Resources.yaz;
+            }
+            else if (secim == 8 || secim == 9 || secim == 10) //Eylül Ekim Kasım
+            {
+                lblMevsim.Text = "Sonbahar";
+                pictureBox1.Image = Properties.Resources.sonbahar;
+            }
+            else  //Diğer geri kalan durumlar
+            {
+                lblMevsim.Text = "Kış";
+                pictureBox1.Image = Properties.Resources.kis;
+            }
+        }
+    }
+}
